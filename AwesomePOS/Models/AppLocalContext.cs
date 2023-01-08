eee27f7d3b48cb1a84f35c9cf8817f4576d27a4d
ashuaebi <ashuaebi@gmail.com>
2023-01-08 09:47:37 +0300

    Add project files.

---
+++ AwesomePOS/Models/AppLocalContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AwesomePOS.Models
+{
+    public class AppLocalContext: DbContext
+    {
+        public AppLocalContext(DbContextOptions<AppLocalContext> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
---
+++ AwesomePOS/Program.cs
@@ -0,0 +1,17 @@
+using AwesomePOS;
+using AwesomePOS.Models;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using SqliteWasmHelper;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddSqliteWasmDbContextFactory<AppLocalContext>(
+    opts => opts.UseSqlite("Data Source=things.sqlite3"));
+await builder.Build().RunAsync();

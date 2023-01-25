ce2a75b3516ddf401b7145ae83f8a2a7e5216184
ashuaebi <ashuaebi@gmail.com>
2023-01-25 19:45:08 +0300

    .

--- AwesomePOS/Models/Product.cs
+++ AwesomePOS/Models/Product.cs
@@ -5,6 +5,7 @@ public class Product
     public int Id { get; set; }
     public string? Name { get; set; } = "";
     public decimal? Price { get; set; } = 0;
+    public decimal? VATRate { get; set; } = 15;
 
     public override string ToString()
     {

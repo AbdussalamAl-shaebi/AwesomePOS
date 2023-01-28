f3c5468d702c21e8c9aa02c3e32e1e6fa4462789
ashuaebi <ashuaebi@gmail.com>
2023-01-28 12:12:53 +0300

    .

--- AwesomePOS/Program.cs
+++ AwesomePOS/Program.cs
@@ -4,6 +4,7 @@ using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor;
 using MudBlazor.Services;
 using SqliteWasmHelper;
 
@@ -22,6 +23,15 @@ builder.Services.AddFluxor(o =>
 builder.Services.AddSingleton<ProductRepository>();
 builder.Services.AddSingleton<BillRepository>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
 
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 1000;
+    config.SnackbarConfiguration.HideTransitionDuration = 100;
+    config.SnackbarConfiguration.ShowTransitionDuration = 100;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Outlined;
+});
 await builder.Build().RunAsync();
